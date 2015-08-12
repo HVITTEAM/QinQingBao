7be331005eebbdbc4f870e17f82c974f0c3f8546
287178790@qq.com <you@example.com>
2015-08-12 16:40:53 +0800

    Initial Commit

---
+++ QinQingBao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QinQingBao
+//
+//  Created by 董徐维 on 15/8/12.
+//  Copyright (c) 2015年 董徐维. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QinQingBao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QinQingBao
+//
+//  Created by 董徐维 on 15/8/12.
+//  Copyright (c) 2015年 董徐维. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
