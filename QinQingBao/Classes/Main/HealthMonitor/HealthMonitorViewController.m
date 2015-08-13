45146cd2e9510c6296d4aeb895b682e90641bf0e
287178790@qq.com <you@example.com>
2015-08-13 08:29:34 +0800

    修改

---
+++ QinQingBao/Classes/Main/HealthMonitor/HealthMonitorViewController.m
@@ -0,0 +1,37 @@
+//
+//  HealthMonitorViewController.m
+//  QinQingBao
+//
+//  Created by 董徐维 on 15/8/13.
+//  Copyright (c) 2015年 董徐维. All rights reserved.
+//
+
+#import "HealthMonitorViewController.h"
+
+@interface HealthMonitorViewController ()
+
+@end
+
+@implementation HealthMonitorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
