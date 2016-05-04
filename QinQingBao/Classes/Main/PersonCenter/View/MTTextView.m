956acfafc77e9f0f1d32554dd297949730e575b2
DM <swyhei8080@qq.com>
2016-05-04 13:54:46 +0800

    MTTextView.m 更新

--- QinQingBao/Classes/Main/PersonCenter/View/MTTextView.m
+++ QinQingBao/Classes/Main/PersonCenter/View/MTTextView.m
@@ -20,24 +20,14 @@
     if (self) {
         self.backgroundColor = [UIColor clearColor];
         
-<<<<<<< HEAD
-        // 添加一个显示提醒文字的label（显示占位文字的label）
-=======
->>>>>>> 1e558a848deb81be2c4ef29eacdd428b1e3f63f7
         UILabel *placehoderLabel = [[UILabel alloc] init];
         placehoderLabel.numberOfLines = 0;
         placehoderLabel.backgroundColor = [UIColor clearColor];
         [self addSubview:placehoderLabel];
         self.placehoderLabel = placehoderLabel;
-<<<<<<< HEAD
         
-        // 设置默认的占位文字颜色
         self.placehoderColor = [UIColor lightGrayColor];
         
-        // 设置默认的字体
-=======
-        self.placehoderColor = [UIColor lightGrayColor];
->>>>>>> 1e558a848deb81be2c4ef29eacdd428b1e3f63f7
         self.font = [UIFont systemFontOfSize:14];
         
         // 监听内部文字改变
@@ -54,11 +44,6 @@
 #pragma mark - 监听文字改变
 - (void)textDidChange
 {
-<<<<<<< HEAD
-    // text属性：只包括普通的文本字符串
-    // attributedText：包括了显示在textView里面的所有内容（表情、text）
-=======
->>>>>>> 1e558a848deb81be2c4ef29eacdd428b1e3f63f7
     self.placehoderLabel.hidden = self.hasText;
 }
 
@@ -113,11 +98,7 @@
     self.placehoderLabel.y = 8;
     self.placehoderLabel.x = 5;
     self.placehoderLabel.width = self.width - 2 * self.placehoderLabel.x;
-<<<<<<< HEAD
-    // 根据文字计算label的高度
-=======
     
->>>>>>> 1e558a848deb81be2c4ef29eacdd428b1e3f63f7
     CGSize maxSize = CGSizeMake(self.placehoderLabel.width, MAXFLOAT);
     CGSize placehoderSize = [self.placehoder sizeWithFont:self.placehoderLabel.font constrainedToSize:maxSize];
     self.placehoderLabel.height = placehoderSize.height;
