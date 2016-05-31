e40231280188c1c5981d319204569fb9099fa9fa
287178790@qq.com <you@example.com>
2016-05-31 16:35:17 +0800

    test

---
+++ QinQingBao/Classes/Main/PersonCenter/Controller/PushMsg/View/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  QinQingBao
+//
+//  Created by 董徐维 on 16/5/31.
+//  Copyright © 2016年 董徐维. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ QinQingBao/Classes/Main/PersonCenter/Controller/PushMsg/View/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  QinQingBao
+//
+//  Created by 董徐维 on 16/5/31.
+//  Copyright © 2016年 董徐维. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
