d0a3824fda8afacfff429aed88a77eed6225dd5f
swy <swyhei8080@qq.com>
2016-06-07 16:43:56 +0800

    增加绑定界面

--- QinQingBao/Classes/Main/PersonCenter/View/MobileBindingView.m
+++ QinQingBao/Classes/Main/PersonCenter/View/MobileBindingView.m
@@ -1,67 +1,64 @@
 //
-//  SMSVerificationView.m
+//  MobileBindingView.m
 //  QinQingBao
 //
-//  Created by shi on 16/5/20.
+//  Created by shi on 16/6/3.
 //  Copyright © 2016年 董徐维. All rights reserved.
 //
 
-#import "SMSVerificationView.h"
-
 #define kSecondCount 60
 
-@interface SMSVerificationView ()
+#import "MobileBindingView.h"
 
-@property (weak, nonatomic) IBOutlet UITextField *verificationNumTextView;      //验证码输入框
+@interface MobileBindingView ()
 
-@property (weak, nonatomic) IBOutlet UIButton *fetchNumberBtn;       //获取验证码按钮
+@property (weak, nonatomic) IBOutlet UITextField *phoneNumField;
 
-@property (weak, nonatomic) IBOutlet UIButton *cancelBtn;
+@property (weak, nonatomic) IBOutlet UITextField *verifyField;
 
-@property (weak, nonatomic) IBOutlet UIButton *confirmBtn;
+@property (weak, nonatomic) IBOutlet UIButton *verifyBtn;
 
-@property (weak, nonatomic) IBOutlet UIView *contentBoxView;     //包含输入框，按钮的View
+@property (weak, nonatomic) IBOutlet UIView *contentBoxView;    //放置输入框，按钮等内容的View
 
-@property (weak, nonatomic) IBOutlet UILabel *promptLb;       //提示信息UILabel
+@property (assign,nonatomic)NSUInteger secondsOfCount;        //到计时秒数
 
-@property (strong,nonatomic)NSTimer *countTimer;
-
-@property (assign,nonatomic)NSInteger secondsOfCount;           //到计时秒数
+@property (strong,nonatomic)NSTimer *countTimer;               //定时器
 
 @end
 
-@implementation SMSVerificationView
+@implementation MobileBindingView
 
 #pragma mark - 周期方法
 /**
- *  创建一个SMSVerificationView并显示到指定view上
+ *  创建一个MobileBindingView并显示到指定view上
  */
-+(SMSVerificationView *)showSMSVerificationViewToView:(UIView *)targetView
++(MobileBindingView *)showMobileBindingViewToView:(UIView *)targetView
 {
-    SMSVerificationView *smsView = [[[NSBundle mainBundle] loadNibNamed:@"SMSVerificationView" owner:nil options:nil] lastObject];
+    MobileBindingView *mobileBindingView = [[[NSBundle mainBundle] loadNibNamed:@"MobileBindingView" owner:nil options:nil] lastObject];
     
-    smsView.frame = targetView.bounds;
-    [targetView addSubview:smsView];
+    mobileBindingView.frame = targetView.bounds;
+    [targetView addSubview:mobileBindingView];
     
-    smsView.contentBoxView.transform = CGAffineTransformMakeScale(0.3, 0.3);
+    mobileBindingView.contentBoxView.transform = CGAffineTransformMakeScale(0.3, 0.3);
     
     [UIView animateWithDuration:0.3 animations:^{
-        smsView.contentBoxView.transform = CGAffineTransformIdentity;
+        mobileBindingView.contentBoxView.transform = CGAffineTransformIdentity;
     }];
     
-    
     //注册键盘通知
-    [[NSNotificationCenter defaultCenter] addObserver:smsView selector:@selector(keyBoardWillShow:) name:UIKeyboardWillShowNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:smsView selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:mobileBindingView selector:@selector(keyBoardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:mobileBindingView selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
     
-    return smsView;
+    return mobileBindingView;
 }
 
 /**
  *  从指定view上删除
  */
--(void)hideSMSVerificationView
+-(void)hideMobileBindingView
 {
+    [self endEditing:YES];
+    
     [UIView animateWithDuration:0.3 animations:^{
         self.contentBoxView.transform = CGAffineTransformMakeScale(0.2, 0.2);
         self.contentBoxView.alpha = 0;
@@ -71,74 +68,68 @@
     
     //删除监听键盘通知
     [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
-
--(void)awakeFromNib
-{
-    [super awakeFromNib];
-
-    self.fetchNumberBtn.layer.borderWidth = 1.0f;
-    self.fetchNumberBtn.layer.cornerRadius = 7.0f;
-    self.fetchNumberBtn.layer.borderColor = HMColor(230, 230, 230).CGColor;
-    [self.fetchNumberBtn setTitleColor:HMColor(220, 220, 220) forState:UIControlStateDisabled];
-    [self.fetchNumberBtn setTitleColor:HMColor(235, 124, 38) forState:UIControlStateNormal];
-    self.cancelBtn.layer.cornerRadius = 7.0f;
-    self.confirmBtn.layer.cornerRadius = 7.0f;
-}
-
-#pragma mark - setter与getter方法
--(void)setPhoneNum:(NSString *)phoneNum
-{
-    _phoneNum = phoneNum;
     
-    NSString *lastNums = [phoneNum substringFromIndex:(phoneNum.length - 4)];
-    self.promptLb.text = [NSString stringWithFormat:@"输入预留手机***%@收到的验证码",lastNums];
+    [self.countTimer invalidate];
+    self.countTimer = nil;
 }
 
 #pragma mark - 事件方法
-/**
- *  点击取消按钮
- */
-- (IBAction)tapCancleBtnAction:(id)sender
+- (IBAction)cancelBtnClick:(id)sender
 {
-    [self hideSMSVerificationView];
+    if (self.tapCancelBtnCallBack) {
+        self.tapCancelBtnCallBack();
+    }
+    
+    [self hideMobileBindingView];
 }
 
 /**
- *  点击确认按钮
+ *  确定按钮被点击
  */
-- (IBAction)tapConfirmBtnAction:(id)sender
+- (IBAction)confirmBtnClick:(id)sender
 {
-    NSString *verificationNum = self.verificationNumTextView.text;
+    NSString *verificationNum = self.verifyField.text;
+    NSString *phoneNum = self.phoneNumField.text;
     
-    if (verificationNum == nil || verificationNum.length == 0) {
-        [NoticeHelper AlertShow:@"请输入验证码" view:nil];
+    if (verificationNum.length == 0 || phoneNum.length == 0) {
+        [NoticeHelper AlertShow:@"请输入手机号码或验证码" view:nil];
+        return;
+    }
+    
+    if (![self validatePhoneNum:self.phoneNumField.text]) {
+        [NoticeHelper AlertShow:@"请输入正确的手机号码" view:self];
         return;
     }
     
     if (self.tapConfirmBtnCallBack) {
-        self.tapConfirmBtnCallBack(self.verificationNumTextView.text);
+        self.tapConfirmBtnCallBack(self.phoneNumField.text,self.verifyField.text);
     }
     
-    [self hideSMSVerificationView];
+    [self hideMobileBindingView];
 }
 
 #pragma mark - 到计时相关
 /**
- * 点击获取验证码按钮
+ * 点击按钮获取验证码
  */
-- (IBAction)tapFetchNumberBtnActio:(id)sender
+- (IBAction)verifyBtnClick:(id)sender
 {
-    if (!self.phoneNum)
-        return [NoticeHelper AlertShow:@"手机号码为空" view:nil];
+    if (self.phoneNumField.text.length == 0)
+        return [NoticeHelper AlertShow:@"手机号码不能为空" view:nil];
+    
+    if (![self validatePhoneNum:self.phoneNumField.text]) {
+        [NoticeHelper AlertShow:@"请输入正确的手机号码" view:self];
+        return;
+    }
+    
     [self.countTimer invalidate];
     self.countTimer = nil;
     
     self.secondsOfCount = kSecondCount;
     
-    self.fetchNumberBtn.enabled = NO;
+    self.verifyBtn.enabled = NO;
     
-    [self fetchNumberFromServices];
+   [self fetchNumberFromServicesWidthPhoneNum:self.phoneNumField.text];
     
     self.countTimer = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(startCount) userInfo:nil repeats:YES];
 }
@@ -151,24 +142,24 @@
     self.secondsOfCount --;
     
     if (self.secondsOfCount <= 0) {
-        [self.fetchNumberBtn setTitle:@"重新获取" forState:UIControlStateNormal];
-         self.fetchNumberBtn.enabled = YES;
+        [self.verifyBtn setTitle:@"重新获取" forState:UIControlStateNormal];
+        self.verifyBtn.enabled = YES;
         [self.countTimer invalidate];
         self.countTimer = nil;
         return;
     }
     
-    [self.fetchNumberBtn setTitle:[NSString stringWithFormat:@"%ld秒",(long)self.secondsOfCount] forState:UIControlStateNormal];
+    [self.verifyBtn setTitle:[NSString stringWithFormat:@"%ld秒",(long)self.secondsOfCount] forState:UIControlStateNormal];
 }
 
 #pragma mark - 网络相关
 /**
  *  向服务器获取验证码
  */
--(void)fetchNumberFromServices
+-(void)fetchNumberFromServicesWidthPhoneNum:(NSString *)phoneNum
 {
     NSDictionary *params =  @{
-                               @"mobile" : self.phoneNum
+                               @"mobile" : phoneNum
                               };
     [CommonRemoteHelper RemoteWithUrl:URL_getsafenum parameters:params type:CommonRemoteTypeGet success:^(NSDictionary *dict, id responseObject) {
         
@@ -180,9 +171,8 @@
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         [NoticeHelper AlertShow:@"获取验证码失败" view:self];
     }];
-
+    
 }
-
 
 #pragma mark - 键盘处理相关方法
 
@@ -192,7 +182,7 @@
     NSDictionary *keyboardDict = [notification userInfo];
     CGRect keybaordFrame = [[keyboardDict objectForKey:UIKeyboardFrameEndUserInfoKey]CGRectValue];
     CGFloat keyboardHeight = keybaordFrame.size.height;
-
+    
     NSInteger animationCurve = [[keyboardDict objectForKey:UIKeyboardAnimationCurveUserInfoKey] integerValue];
     CGFloat duration = [[keyboardDict objectForKey:UIKeyboardAnimationDurationUserInfoKey] floatValue];
     
@@ -206,7 +196,7 @@
     
     //条件成立，说明键盘遮挡了文本框
     if (contentBoxViewMaxY + 60 >= keyboardTopToViewTop) {
-
+        
         [UIView animateWithDuration:duration delay:0 options:animationCurve animations:^{
             self.contentBoxView.transform = CGAffineTransformTranslate(self.contentBoxView.transform, 0, -(contentBoxViewMaxY + 60 - keyboardTopToViewTop));
         } completion:nil];
@@ -223,6 +213,18 @@
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     [self endEditing:YES];
+}
+
+#pragma mark - 工具方法
+/**
+ *  验证手机号是否合法格式,合法返回YES，否则为NO
+ */
+-(BOOL)validatePhoneNum:(NSString *)phone
+{
+    //判断手机号码是否正确
+    NSString *expression = @"^1[3578]\\d{9}$";
+    NSPredicate *prediccate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",expression];
+    return [prediccate evaluateWithObject:phone];
 }
 
 @end
